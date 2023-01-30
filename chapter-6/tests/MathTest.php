58abd1ce6ca06f162d12a727ef2df14327b5e4af
kattsun44 <kattsun4416@gmail.com>
2023-01-31 07:13:54 +0900

    p127 より良いテストコード
    - min と max は別のテストシナリオとして分けておく
    - 境界値でテストを行う

--- chapter-6/tests/MathTest.php
+++ chapter-6/tests/MathTest.php
@@ -7,16 +7,26 @@ require_once __DIR__ . '/../src/Math.php';
 
 class MathTest extends TestCase
 {
-    public function testMinMax(): void
+    public function testMin(): void
     {
         $math = new Math();
-
-        $minExpected = 1;
-        $minResult = $math->min(1, 2);
-        $this->assertEquals($minExpected, $minResult);
-
-        $maxExpected = 2;
-        $maxResult = $math->max(1, 2);
-        $this->assertEquals($maxExpected, $maxResult);
+        $this->assertEquals(0, $math->min(0, 1));
+        $this->assertEquals(0, $math->min(1, 0));
+        $this->assertEquals(-1, $math->min(0, -1));
+        $this->assertEquals(-1, $math->min(-1, 0));
+        $this->assertEquals(0, $math->min(0, 0));
+        $this->assertEquals(0, $math->min(0, PHP_INT_MAX));
+        $this->assertEquals(PHP_INT_MIN, $math->min(0, PHP_INT_MIN));
+    }
+    public function testMax(): void
+    {
+        $math = new Math();
+        $this->assertEquals(1, $math->max(0, 1));
+        $this->assertEquals(1, $math->max(1, 0));
+        $this->assertEquals(0, $math->max(0, -1));
+        $this->assertEquals(0, $math->max(-1, 0));
+        $this->assertEquals(0, $math->max(0, 0));
+        $this->assertEquals(PHP_INT_MAX, $math->max(0, PHP_INT_MAX));
+        $this->assertEquals(0, $math->max(0, PHP_INT_MIN));
     }
 }
