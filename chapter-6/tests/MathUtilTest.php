9219e44fb0130ce0e23741ee041accca4871350c
kattsun44 <kattsun4416@gmail.com>
2023-02-12 18:21:05 +0900

    p133 モックオブジェクト
    6-11 モックオブジェクトの例

--- chapter-6/tests/MathUtilTest.php
+++ chapter-6/tests/MathUtilTest.php
@@ -10,13 +10,23 @@ class MathUtilTest extends TestCase
 {
     public function testSaturate(): void
     {
-        $mathStab = $this->createMock(Math::class);
-        $mathUtil = new MathUtil($mathStab);
+        $math = $this->createMock(Math::class);
+        $mathUtil = new MathUtil($math);
 
-        $mathStab->method('max')->willReturn(2);
-        $mathStab->method('min')->willReturn(1);
+        // 少なくとも1回 max が引数 2, 1 で呼ばれ、2 を返す
+        $math->expects($this->atLeastOnce())
+            ->method('max')
+            ->with($this->equalTo(2), $this->equalTo(1))
+            ->willReturn(2);
+
+        // 少なくとも1回 min が引数 2, 3 で呼ばれ、2 を返す
+        $math->expects($this->atLeastOnce())
+            ->method('min')
+            ->with($this->equalTo(2), $this->equalTo(3))
+            ->willReturn(2);
+
+
         $result = $mathUtil->saturate(2, 1, 3);
-
         $this->assertEquals(2, $result);
     }
 }
